f1776f8e7ee666c267b715a92b373c30b19c3947
s0lana69 <rami.janabi91@gmail.com>
2025-04-30 02:25:06 +0200

    Copy src/pages/plasmic-test.tsx from omg

---
+++ src/pages/plasmic-test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PlasmicComponent from '../components/PlasmicComponent';
+
+export default function PlasmicTestPage() {
+  return (
+    <div>
+      <h1>Plasmic Test Page</h1>
+      <PlasmicComponent component="Homepage" />
+    </div>
+  );
+} 
