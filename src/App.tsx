84930d6911740c7399029f8700e2f3c1a0a204ca
s0lana69 <rami.janabi91@gmail.com>
2025-04-30 02:24:41 +0200

    Copy src/App.tsx from omg

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my website</h1>
+      <p>This is a simple Next.js application</p>
+    </div>
+  );
+}
+
+export default App;
